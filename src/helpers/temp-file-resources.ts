cee63e43b2c67636f1bc0e643f60e114d4a8ad6c
apyakushevskiy <apyakushevskiy@avito.ru>
2025-04-13 01:19:57 +0500

    Заменил интервалы на ивенты при захвате временного файла

--- src/helpers/temp-file-resources.ts
+++ src/helpers/temp-file-resources.ts
@@ -1,26 +1,39 @@
 import fs from 'fs';
+import EventEmitter from 'node:events';
 
 const FILES_USAGE = new Set<string>();
 
+const CUSTOM_EVENT_NAME = 'tempFileRealease';
+
+const eventEmitter = new EventEmitter();
+
 export const captureTempFile = async (fileName: string) => {
-    // TODO: events
     if (!FILES_USAGE.has(fileName)) {
         FILES_USAGE.add(fileName);
         return;
     }
 
     await new Promise<void>((resolve) => {
-        const intervalId = setInterval(() => {
-            if (!FILES_USAGE.has(fileName)) {
-                FILES_USAGE.add(fileName);
-                clearInterval(intervalId);
-                return resolve();
+        const listener = (releasedFileName: string) => {
+            if (releasedFileName !== fileName) {
+                return;
             }
-        }, 50);
+
+            if (FILES_USAGE.has(fileName)) {
+                return;
+            }
+
+            FILES_USAGE.add(fileName);
+            eventEmitter.removeListener(CUSTOM_EVENT_NAME, listener);
+            return resolve();
+        };
+
+        eventEmitter.addListener(CUSTOM_EVENT_NAME, listener);
     });
 };
 
 export const clenupTempFile = (fileName: string) => {
     fs.rmSync(fileName);
     FILES_USAGE.delete(fileName);
+    eventEmitter.emit(CUSTOM_EVENT_NAME, fileName);
 };
